a2b962ac9d4b40a919ee96a72c59dc7c21bc47e1
mahimagawale <115380842+mahimagawale@users.noreply.github.com>
2022-11-28 19:08:59 +0530

    Add files via upload

---
+++ problem_solving/Drawing_Book.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+     
+     public static int pageCount(int n, int p) {
+    // Write your code here
+       int df = p/2;
+       int de=n/2;
+    return Math.min(df,(de-df));
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
